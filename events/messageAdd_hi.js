f37b3198863eb7431aeed9e40fd1f03b73206dd0
Lyuun <112559239+Lyuuniversal@users.noreply.github.com>
2023-04-01 18:59:21 +0630

    Update messageAdd_hi.js

--- events/messageAdd_hi.js
+++ events/messageAdd_hi.js
@@ -4,14 +4,14 @@ const client = require('../index');
 console.log('[Event] Loaded messageAdd_hi.js')
 
 client.on(Events.MessageCreate, (message) => {
-    const p = ['hello', 'hi', 'hey', 'yo', 'heya']
+  const keywords = ['hello', 'hi', 'hey', 'yo', 'heya'];
 
-    if (message.author.bot || !message.guild) {
-      return;
-    }
+  if (message.author.bot || !message.guild) {
+    return;
+  }
 
-    if (p.some(word => message.content.toLowerCase().includes(word))) {
-      message.reply('Hello!<:elysia_wave:1091633194685702144>');
-    }
-  });
-  
+  const content = message.content.toLowerCase();
+  if (keywords.includes(content)) {
+    message.reply('Hello!<:elysia_wave:1091633194685702144>');
+  }
+});
