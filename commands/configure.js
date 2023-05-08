6bcbbb3f5fabdda7a1408ef4afcbb1c1d194a2ad
Lyuun <112559239+Lyuuniversal@users.noreply.github.com>
2023-05-08 19:36:07 +0630

    k

--- commands/configure.js
+++ commands/configure.js
@@ -40,11 +40,22 @@ module.exports = {
             .setDescription("The commands name to disable")
             .setRequired(true)
         )
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("enable")
+        .setDescription("Enable disabled commands")
+        .addStringOption((option) =>
+          option
+            .setName("command")
+            .setDescription("The commands name to enable")
+            .setRequired(true)
+        )
     ),
   async execute(interaction) {
-    if (
-      !interaction.member.permissions.has(PermissionFlagsBits.Administrator)
-    ) {
+    delete require.cache[require.resolve("../utils/bot_managers.json")];
+    const { managers } = require("../utils/bot_managers.json");
+    if (!managers.includes(interaction.user.id)) {
       console.log(
         `[Command_Handling] ${interaction.user.tag} denied to use command: /configure`
       );
@@ -60,7 +71,7 @@ module.exports = {
     // ---------------------------------  Disable  ---------------------------------  //
 
     if (sub === "disable") {
-      const why = interaction.options.getString('command')
+      const why = interaction.options.getString("command");
       const huh = path.join(__dirname, "../utils/command_config.json");
       const configFile = fs.readFileSync(huh);
       const config = JSON.parse(configFile);
@@ -69,12 +80,43 @@ module.exports = {
         return;
       }
       const hi = new EmbedBuilder()
-      .setTitle("✦ Configuration : Disabling Commands")
-      .setDescription(`Successfully disabled **/${why}**!`)
-      .setTimestamp();
-      config[why] = "false"
-      fs.writeFileSync(huh, JSON.stringify(config));
-      interaction.reply({embeds: [hi]})
+        .setTitle("✦ Configuration : Disabling Commands")
+        .setDescription(`Successfully disabled **/${why}**!`)
+        .setTimestamp();
+        if (config[why] == "false") {
+          interaction.reply(`**${why}** is already disabled!`);
+        } else {
+          config[why] = "false";
+          fs.writeFileSync(huh, JSON.stringify(config));
+          console.log(`[Command_Handling] Disabled /${why}`);
+          interaction.reply({ embeds: [hi] });
+        }
+    }
+
+    // ---------------------------------  Enable  ---------------------------------  //
+
+    if (sub === "enable") {
+      const why = interaction.options.getString("command");
+      const huh = path.join(__dirname, "../utils/command_config.json");
+      const configFile = fs.readFileSync(huh);
+      const config = JSON.parse(configFile);
+      if (!(why in config)) {
+        interaction.reply(`Invalid command name: **${why}**`);
+        return;
+      }
+      const hi = new EmbedBuilder()
+        .setTitle("✦ Configuration : Enabling Commands")
+        .setDescription(`Successfully enabled **/${why}**!`)
+        .setTimestamp();
+
+      if (config[why] == "true") {
+        interaction.reply(`**${why}** is already enabled!`);
+      } else {
+        config[why] = "true";
+        fs.writeFileSync(huh, JSON.stringify(config));
+        console.log(`[Command_Handling] Enabled /${why}`);
+        interaction.reply({ embeds: [hi] });
+      }
     }
 
     // ---------------------------------  IDs  ---------------------------------  //
@@ -108,8 +150,7 @@ module.exports = {
       const p = new EmbedBuilder()
         .setTitle("✦ Configuration : Update")
         .setDescription(`Successfully changed **${o}**'s value to ${u}`)
-        .setTimestamp()
-        .setColor("#73a6ff");
+        .setTimestamp();
       if (!o) {
         await interaction.reply("Config ID not found.");
       } else if (o === "c01") {
