eafa881dbb29f11f0cb9d42f3735ef8f0720dc0e
Lyuuniversal <kaisolovesjelly@gmail.com>
2023-05-28 22:19:12 +0630

    omgfix

--- index.js
+++ index.js
@@ -90,6 +90,17 @@ function doShellCmd(command) {
   }
 }
 
+// bot.refresh
+process.stdin.on("data", (data) => {
+  const input = data.toString().trim();
+  if (input == "bot.refresh") {
+    console.log('[Bot] Restarting...')
+    client.destroy()
+    client.login(token)
+    console.log('[Bot] Restart complete!')
+  }
+});
+
 // bot.Data.update
 process.stdin.on("data", (data) => {
   const input = data.toString().trim(); 
@@ -103,34 +114,40 @@ process.stdin.on("data", (data) => {
 });
 
 // bot.Data.view
+// bot.Data.view
 process.stdin.on("data", (data) => {
-	const input = data.toString().trim(); 
-	const args = input.split(" "); // LYUU FINDS OUT ABOUT ARGS AND ITS THE BEST THING EVER!>>!?!?!?
-	if (args[0] === "bot.Data.view") {
-	  const command = args[0];
-	  const id = args[1];
+  const input = data.toString().trim();
+  const args = input.split(" "); // LYUU FINDS OUT ABOUT ARGS AND ITS THE BEST THING EVER!>>!?!?!?
+  if (args[0] === "bot.Data.view") {
+    const command = args[0];
+    const id = args[1];
 
-	  const usernameData = require('./data/username.json')
-	  const profileData =  require('./data/profile.json')
-	  const likesData =  require('./data/likes.json')
+    const usernameData = require("./data/username.json");
+    const profileData = require("./data/profile.json");
+    const likesData = require("./data/likes.json");
 
-	  if (usernameData[id] == undefined){
-		console.log(`[Data] ${id} not found within data.`)
-		return;
-	  }
+    if (usernameData[id] == undefined) {
+      console.log(`[Data] ${id} not found within data.\n`);
+      return;
+    }
 
-	  console.log(`----------- Viewing data... ----------\n`)
-	  console.log(`[Data/Username] ${usernameData[id]}`)
-	  console.log(`[Data/likesData] ${likesData[`${id}_likes`]} likes`)
-	  console.log(`[Data/likesData] ${likesData[`${id}_liked`]} liked`)
-	  console.log(`[Data/ProfileData] ${profileData[`${id}_color`]}`)
-	  console.log(`[Data/ProfileData] ${profileData[`${id}_desc`]}`)
-	  console.log(`[Data/ProfileData] ${profileData[`${id}_url`]}`)
-	  console.log(`[Data/ProfileData] ${profileData[`${id}_icon`]}`)
-	  console.log(`[Data/ProfileData] ${profileData[`${id}_footer`]}`)
-	  console.log(`[Data/ProfileData] ${profileData[`${id}_ficon`]}`)
-	}
-  });
+    console.log(`----------- ${usernameData[id]} in data ----------\n`);
+    console.log(`[Data/likesData] ${likesData[`${id}_likes`]} likes`);
+    console.log(`[Data/likesData] ${likesData[`${id}_liked`]} liked`);
+    console.log(
+      `[Data/ProfileData] Embed Color: ${profileData[`${id}_color`]}`
+    );
+    console.log(`[Data/ProfileData] Description: ${profileData[`${id}_desc`]}`);
+    console.log(`[Data/ProfileData] Banner Url: ${profileData[`${id}_url`]}`);
+    console.log(`[Data/ProfileData] Icon Url: ${profileData[`${id}_icon`]}`);
+    console.log(
+      `[Data/ProfileData] Footer Text: ${profileData[`${id}_footer`]}`
+    );
+    console.log(
+      `[Data/ProfileData] Footer Icon: ${profileData[`${id}_ficon\n`]}`
+    );
+  }
+});
   
 
 // ---------------------------------  Deploy and login  ---------------------------------  //
